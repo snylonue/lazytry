9b387f16277792ea256951f85cea9a4d27e2b7e7
snylonue <snylonue@gmail.com>
2022-07-26 15:25:48 +0800

    feat(sync): migrate from unsync

--- src/sync.rs
+++ src/sync.rs
@@ -1,29 +1,57 @@
 use once_cell::sync::OnceCell;
-use std::{cell::Cell, error::Error, marker::PhantomData};
+use std::cell::Cell;
 
-pub struct LazyTry<T, E = Box<dyn std::error::Error + Sync>, F = fn() -> Result<T, E>> {
+use crate::error::Error;
+
+pub struct LazyTry<T, F> {
     cell: OnceCell<T>,
     init: Cell<Option<F>>,
-    _marker: PhantomData<E>,
 }
 
-unsafe impl<T, E: Error + Sync, F: Send> Sync for LazyTry<T, E, F> where OnceCell<T>: Sync {}
+unsafe impl<T, F> Sync for LazyTry<T, F> where OnceCell<T>: Sync {}
 
-impl<T, E, F: FnOnce() -> Result<T, E>> LazyTry<T, E, F> {
+impl<T, E, F: FnOnce() -> Result<T, E>> LazyTry<T, F> {
     pub const fn new(f: F) -> Self {
         Self {
             cell: OnceCell::new(),
             init: Cell::new(Some(f)),
-            _marker: PhantomData,
         }
     }
 
     /// # Panic
     /// when `force()` has been called before and retured an `Err`
-    pub fn force(&self) -> Result<&T, E> {
+    pub fn force(&self) -> Result<&T, Error<E>> {
         self.cell.get_or_try_init(|| match self.init.take() {
-            Some(f) => f(),
-            None => panic!("Lazy instance has previously been poisoned"),
+            Some(f) => f().map_err(Error::Failed),
+            None => Err(Error::Poisoned),
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::num::{IntErrorKind, ParseIntError};
+
+    use super::*;
+
+    #[test]
+    fn lazy_try_force() {
+        static LAZY: LazyTry<i32, fn() -> Result<i32, ParseIntError>> =
+            LazyTry::new(|| "1".parse());
+
+        assert_eq!(LAZY.force().unwrap(), &1);
+        assert_eq!(unsafe { *LAZY.force().unwrap_unchecked() }, 1);
+    }
+
+    #[test]
+    fn lazy_try_force_err() {
+        static LAZY: LazyTry<i32, fn() -> Result<i32, ParseIntError>> =
+            LazyTry::new(|| "a".parse());
+
+        assert_eq!(
+            *LAZY.force().unwrap_err().into_err().unwrap().kind(),
+            IntErrorKind::InvalidDigit
+        );
+        assert_eq!(LAZY.force().unwrap_err(), Error::Poisoned);
     }
 }
