00ec9012772ff6a4a69edfb7aad370b77139f303
snylonue <snylonue@gmail.com>
2022-07-26 14:15:36 +0800

    test: move to unsync

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,8 @@
-pub mod unsync;
 pub mod sync;
+pub mod unsync;
 
 use std::error::Error;
 
 pub fn into_box_err(e: impl Error + Sync + 'static) -> Box<dyn Error + Sync> {
     Box::new(e)
-}
-
-#[cfg(test)]
-mod tests {
-    use std::num::IntErrorKind;
-
-    use super::*;
-    use unsync::LazyTry;
-
-    #[test]
-    fn lazy_try_force() {
-        let lazy: LazyTry<i32, _> = LazyTry::new(|| "1".parse());
-
-        assert_eq!(lazy.force().unwrap(), &1);
-        assert_eq!(unsafe { *lazy.force().unwrap_unchecked() }, 1);
-    }
-
-    #[test]
-    fn lazy_try_force_err() {
-        let lazy: LazyTry<i32, _> = LazyTry::new(|| "a".parse());
-
-        assert_eq!(
-            *lazy.force().unwrap_err().kind(),
-            IntErrorKind::InvalidDigit
-        );
-    }
 }
--- src/unsync.rs
+++ src/unsync.rs
@@ -27,3 +27,28 @@ impl<T, E, F: FnOnce() -> Result<T, E>> LazyTry<T, F> {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::num::IntErrorKind;
+
+    use super::*;
+
+    #[test]
+    fn lazy_try_force() {
+        let lazy: LazyTry<i32, _> = LazyTry::new(|| "1".parse());
+
+        assert_eq!(lazy.force().unwrap(), &1);
+        assert_eq!(unsafe { *lazy.force().unwrap_unchecked() }, 1);
+    }
+
+    #[test]
+    fn lazy_try_force_err() {
+        let lazy: LazyTry<i32, _> = LazyTry::new(|| "a".parse());
+
+        assert_eq!(
+            *lazy.force().unwrap_err().kind(),
+            IntErrorKind::InvalidDigit
+        );
+    }
+}
